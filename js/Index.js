2f021a8f93fb47cfad26732f62dafcf43e292961
amwabale <a.m.wabale.technical@gmail.com>
2017-07-23 11:54:07 +0530

    Added project images

--- js/Index.js
+++ js/Index.js
@@ -1,13 +1,22 @@
 var Index = new function () {
     var self = this;
+    this.Release = false;
 
     this.Ready = function () {
         
         var winHt = $(window).height();
         $(".BannerImage").height(winHt);
 
-        
-        $(".freebirdFormviewerViewFormBanner").css({ "display": "none" });
+        if(self.Release == true)
+        {
+            $("#debug").css({ "display": "none" })
+            $("#release").css({ "display": "block" })
+        }
+        else
+        {
+            $("#release").css({ "display": "none" })
+            $("#release").remove();
+        }
     }
 }
 $(document).ready(Index.Ready);
